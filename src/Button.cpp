4332fdd6196da93619edab35f5665d64a03ed708
SpeedyTheXD <tranlinh4122005@gmail.com>
2025-12-15 17:49:08 +0700

    Changed the button logic

--- src/Button.cpp
+++ src/Button.cpp
@@ -1,6 +1,6 @@
 #include "Button.h"
 
-//Button declaration
+// Button declaration
 Button::Button(int pin, bool activeHigh)
     : pin(pin), activeHigh(activeHigh) {}
 
@@ -9,4 +9,44 @@ void Button::setup()
     pinMode(pin, INPUT); // external pulldown → correct
 }
 
+void Button::loop()
+{
+    bool raw = digitalRead(pin);
 
+    bool logical;
+    if (activeHigh)
+    {
+        logical = raw;
+    }
+    else
+    {
+        logical = !raw;
+    }
+
+    if (logical != lastState)
+    {
+        lastChange = millis();
+    }
+
+    if ((millis() - lastChange) > debounceMs)
+    {
+        if (logical && !pressedEvent && !stableState)
+        {
+            pressedEvent = true;
+        }
+        stableState = logical;
+    }
+
+    lastState = logical;
+}
+
+bool Button::wasPressed()
+{
+    if (pressedEvent)
+    {
+        pressedEvent = false;
+        return true;
+    }
+
+    return false;
+}
