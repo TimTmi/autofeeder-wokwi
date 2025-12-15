94edc3840013d9e667c4e4fae6bac3f09b529b8d
SpeedyTheXD <tranlinh4122005@gmail.com>
2025-12-15 17:45:12 +0700

    Remake button

--- src/Button.cpp
+++ src/Button.cpp
@@ -1,34 +1,12 @@
 #include "Button.h"
 
+//Button declaration
 Button::Button(int pin, bool activeHigh)
     : pin(pin), activeHigh(activeHigh) {}
 
-void Button::setup() {
-    pinMode(pin, INPUT);  // external pulldown → correct
+void Button::setup()
+{
+    pinMode(pin, INPUT); // external pulldown → correct
 }
 
-void Button::loop() {
-    bool raw = digitalRead(pin);
-    bool logical = activeHigh ? raw : !raw;
 
-    if (logical != lastState) {
-        lastChange = millis();
-    }
-
-    if ((millis() - lastChange) > debounceMs) {
-        if (logical && !pressedEvent && !stableState) {
-            pressedEvent = true;
-        }
-        stableState = logical;
-    }
-
-    lastState = logical;
-}
-
-bool Button::wasPressed() {
-    if (pressedEvent) {
-        pressedEvent = false;
-        return true;
-    }
-    return false;
-}
